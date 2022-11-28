ef2f10bfac0cc158021ef38e8dc1778e056c6e03
Evgenii <evgenypetrov5230@gmail.com>
2022-11-28 14:14:13 +0300

    fix + ref

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
 const play = document.querySelector('.about__play'),
-   video = document.querySelector('.about__video video'),
-   burger = document.querySelector('.burger-menu'),
-   menu = document.querySelector('.menu');
+	video = document.querySelector('.about__video video'),
+	burger = document.querySelector('.burger-menu'),
+	menu = document.querySelector('.menu');
 
 play.addEventListener('click', () => {
-   video.play();
-   video.setAttribute('controls', 'controls');
-   play.classList.add('about__play--hidden');
+	video.play();
+	video.setAttribute('controls', 'controls');
+	play.classList.add('about__play--hidden');
 });
 
-burger.onclick = (e) => {
-   burger.classList.toggle('active');
-   menu.classList.toggle('active');
-   document.querySelector('body').classList.toggle('lock');
-};
+burger.addEventListener('click', () => {
+	burger.classList.toggle('active');
+	menu.classList.toggle('active');
+	document.querySelector('body').classList.toggle('lock');
+});
